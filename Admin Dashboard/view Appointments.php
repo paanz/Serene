615346ca6ff0ed0a26e629445ce0464df6b20e95
Najimjim <83295616+Najimjim@users.noreply.github.com>
2023-01-05 14:41:01 +0800

    Update view Appointments.php

--- Admin Dashboard/view Appointments.php
+++ Admin Dashboard/view Appointments.php
@@ -278,7 +278,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Booking Details</h1>
+                            <h1 class="m-0">Appointment Details</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
