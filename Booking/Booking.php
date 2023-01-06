643c5439af8409f7855634064079f497baf72ec2
Najimjim <83295616+Najimjim@users.noreply.github.com>
2023-01-06 22:10:15 +0800

    Update Booking.php

--- Booking/Booking.php
+++ Booking/Booking.php
@@ -2,12 +2,12 @@
 	require 'connection_booking.php';
 	
 	if(isset($_POST['submit'])){
-	$cust_Name = $_POST['fullname'];
-	$cust_Email = $_POST['email'];
-	$cust_phone = $_POST['mobile'];
-	$property_Name = $_POST['PropertyName'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$PropertyName = $_POST['PropertyName'];
 	
-	$query = "INSERT INTO booking VALUES ('','$cust_Name', '$cust_phone', '$cust_Email', '$property_Name')";
+	$query = "INSERT INTO booking VALUES ('','$name', '$phone', '$email', '$PropertyName')";
 	mysqli_query($conn,$query);
 	echo
   "
@@ -35,14 +35,14 @@
 			function validation(){
 
 			// Validate Email
-			var cust_Email = document.forms["myform"]["cust_Email"].value;
-			if(cust_Email==''){
+			var email = document.forms["myform"]["email"].value;
+			if(email==''){
 				alert("Please enter your email.");
 				return false;
 			}
 			else{
 				var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-				var x = re.test(cust_Email);
+				var x = re.test(email);
 				if(x){}
 			else{
 				alert("Email should be in proper format (e.g xxx@xxxx.xxx)");
@@ -51,14 +51,14 @@
 		}
 	
 			// Validate Mobile
-			var cust_phone = document.forms["myform"]["cust_phone"].value;
-			if(cust_phone==''){
+			var phone = document.forms["myform"]["phone"].value;
+			if(phone==''){
 				alert("Please enter your mobile number.");
 				return false;
 			}
 			else{
 				var re = /^[\d]{3}[\d]{7,7}$/;
-				var x = re.test(cust_phone);
+				var x = re.test(phone);
 				if(x){}
 			else{
 				alert("Mobile number should be in proper format (e.g 0101234567)");
@@ -90,22 +90,22 @@
 						
 						    <div class="form-group">
 							    <label>Full Name</label>
-							    <input type="text" id="cust_Name" name="fullname" class="form-control" value="" placeholder="Enter Your First Name" required>			
+							    <input type="text" id="name" name="name" class="form-control" value="" placeholder="Enter Your First Name" required>			
 						    </div>
 						 
 						    <div class="form-group">
 						    	<label>Email</label>
-						    	<input type="text" id="cust_Email" name="email" class="form-control" value="" placeholder="Enter Your Email" required>
+						    	<input type="text" id="email" name="email" class="form-control" value="" placeholder="Enter Your Email" required>
 						    </div>
 							
 						    <div class="form-group">
 						     	<label>Mobile</label>
-						    	<input type="text" id="cust_phone" name="mobile" class="form-control" value="" placeholder="Enter Your Mobile number" required>
+						    	<input type="text" id="phone" name="phone" class="form-control" value="" placeholder="Enter Your Mobile number" required>
 						    </div>
 							
 							<div class="form-group">
 						     	<label>Select Property Type</label>
-								<select name="PropertyName" id="property_Name" value="" style="width:100%; height:30px; cursor: pointer;" required>
+								<select name="PropertyName" id="PropertyName" value="" style="width:100%; height:30px; cursor: pointer;" required>
 								    <option value="" selected hidden>Select Property</option>
 									<option value="Detached Double Storey">Detached Double Storey</option>
 									<option value="Semi-Detached Double Storey">Semi-Detached Double Storey</option>
@@ -124,8 +124,47 @@
                             </div>
 							
 							<div class="form-group m-0">
-                                <button type="submit" name="submit" class="btn btn-primary btn-block" >Book Now</button>
+                                <button type="submit" name="submit" onclick="sendEmail()" value="Send An Email" class="btn btn-primary btn-block" >Book Now</button>
                             </div>
+							
+							 <!--====== MAILER PART START ======-->    
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>                   
+	<script type="text/javascript">
+        function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var phone = $("#phone");
+			var PropertyName = $("#PropertyName");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(phone) && isNotEmpty(PropertyName)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       phone: phone.val(),
+                       PropertyName: PropertyName.val(),
+                   }, success: function (response) {
+                        $('#myForm')[0].reset();
+                        alert("Message Sent Successfully.");
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+    </script>
+    <!--====== MAILER PART ENDS ======--> 
 					
 					    </form>
 					</div>
@@ -135,10 +174,6 @@
 		    </div>	
         </section>	
 
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      
 	</body>
 
 </html>
